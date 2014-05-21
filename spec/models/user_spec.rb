1e502c661dc22f8d3e2450b8bfa2c1ce9f9523dc
jayaganthan <jayaganthanjk@gmail.com>
2014-05-21 12:12:29 +0530

    chapter 6

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,11 +2,15 @@ require 'spec_helper'
 
 describe User do
   before do
-    @user = User.new(name: "Check ", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
   end
   subject { @user }
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
   it { should be_valid }
   describe "when name is not present" do
     before { @user.name = " " }
@@ -15,5 +19,20 @@ describe User do
   describe "When name s too long " do
   	before { @user.name = "a" * 51 }
   	it { should_not be_valid }
+  end
+  describe "return value of authenticate method" do
+    before { @user.save }
+    let(:found_user) { User.find_by(email: @user.email) }
+
+    describe "with valid password" do
+      it { should eq found_user.authenticate(@user.password) }
+    end
+
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+      it { should_not eq user_for_invalid_password }
+      specify { expect(user_for_invalid_password).to be_false }
+    end
   end
 end
