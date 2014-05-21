b3439ac6935ef7710decd8e6884a6af56661208f
jayaganthan <jayaganthanjk@gmail.com>
2014-05-21 15:44:14 +0530

    chapter 7

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe "Static pages" do
   describe "Home page" do
-    it "should have the content 'Sample App'" do
+    it "should have the content 'home'" do
       visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
+      expect(page).to have_content('home')
     end
     it "should have the title 'Tutorial | Home'" do
     	visit '/static_pages/home'
@@ -20,7 +20,7 @@ describe "Static pages" do
   describe "About us" do
   	it "should have the content 'About us'" do
   		visit '/static_pages/about'
-  		expect(page).to have_content('about us')
+  		expect(page).to have_content('aboutus')
   	end
   end
   describe "contact" do
@@ -30,3 +30,34 @@ describe "Static pages" do
   	end
   end
 end
+describe "User Pages" do
+  subject {page}
+  describe "Signup page" do
+    before {visit signup_path }
+    it { should have_content('signup page')}
+    it { should have_title(full_title('signup')) }
+  end
+  describe "Profile page" do
+    before { visit user_path(user) }
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+  end
+  visit signup_path
+    fill_in "Name",         with: "Example User"
+    fill_in "Email",        with: "user@example.com"
+    fill_in "Password",     with: "foobar"
+    fill_in "Confirmation", with: "foobar"
+    click_button "Create my account"
+   describe "with valid information" do
+    before do
+      fill_in "Name",         with: "Example User"
+      fill_in "Email",        with: "user@example.com"
+      fill_in "Password",     with: "foobar"
+      fill_in "Confirmation", with: "foobar"
+    end
+
+    it "should create a user" do
+      expect { click_button submit }.to change(User, :count).by(1)
+    end
+  end
+end 
